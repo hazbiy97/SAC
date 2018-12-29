326d399ea034dc33dea678b99cd44c40c2297a8d
Unknown <nur.hazbiy@yahoo.com>
2018-12-29 14:12:29 +0700

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/satc/';
+$config['base_url'] = 'http://localhost/sac/';
 
 /*
 |--------------------------------------------------------------------------
